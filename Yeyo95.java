3e2850498654ca040c3a44dc8fdedfd6f39a8463
Marvin Rodriguez <130935620+Yeyo95@users.noreply.github.com>
2024-04-27 18:05:19 -0600

    Add files via upload

---
+++ Yeyo95.java
@@ -0,0 +1,34 @@
+// Este es el sitio web oficial de java https://www.java.com/es/
+
+// Sintaxis de comentarios en java
+
+// Este es un comentario en una linea en java
+
+/* este es un comentario
+ multilinea en java */
+
+
+
+public class Yeyo95 {
+    public static void main(String[] args) {
+
+        // Tipos de datos primitivos
+        String texto = "Mi primer texto";
+        int edad = 18;
+        boolean miBooleano = false;
+        double decimal = 10.11;
+        float miFloat = 2.55f;
+        char miCaracter = 'M';
+
+        // Variable
+
+        int numero = 20;
+        // Constante
+        final String nombreLeng = "Java!";
+
+
+        // Imprimir en terminal el texto
+
+        System.out.println("Hola, "+nombreLeng);
+    }
+}
